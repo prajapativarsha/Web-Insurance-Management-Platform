982fe6b4df355f3fd8a05a2326e399943058005a
VARSHA PRAJAPATI <prajapativarsha488@gmail.com>
2026-07-22 19:37:14 +0530

    Developed frontend for authentication module

---
+++ frontend/src/components/ui/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loader = ({ className = '' }) => {
+  return (
+    <div className="flex justify-center items-center ">
+      <div className={`animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-primary ${className}`}></div>
+    </div>
+  );
+};
+
+export default Loader;
