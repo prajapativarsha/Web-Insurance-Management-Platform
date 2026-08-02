b5d82451555b75130092103b264bd66bc36b494c
VARSHA PRAJAPATI <prajapativarsha488@gmail.com>
2026-08-02 15:38:57 +0530

    Changed the base url in frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create an Axios instance with your backend's base URL
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api/v1', // Ensure this matches your backend port
+  baseURL: 'https://web-insurance-management-platform-ytg4.onrender.com/api/v1', // Ensure this matches your backend port
 });
 API.interceptors.request.use((config) => {
     const token = localStorage.getItem('token'); // Or wherever you store your token
